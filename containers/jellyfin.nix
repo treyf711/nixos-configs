54748b502eb5213f641a6feb7459c8672b322cf2
Trey Fordham <treyf711+github@gmail.com>
2024-03-11 12:22:52 -0400

    allow jellyfin to use hardware {de,en}coding

--- containers/jellyfin.nix
+++ containers/jellyfin.nix
@@ -5,15 +5,30 @@
     privateNetwork = true;
     hostAddress = "192.168.0.4";
     localAddress = "10.0.0.5";
+    allowedDevices = [
+      {
+        modifier = "rw";
+	node = "/dev/dri/renderD128";
+      }
+    ];
     bindMounts = {
       "/media" = { 
         hostPath = "/storage/media";
         isReadOnly = false;
       };
+      "/dev/dri/renderD128" = {
+        hostPath = "/dev/dri/renderD128";
+        isReadOnly = false;
+      };
     };
 
     config = { config, pkgs, lib, ...}: {
-
+      hardware.opengl = {
+        enable = true;
+        extraPackages = [
+          pkgs.vaapiVdpau
+        ];
+      };
       services.jellyfin = {
         enable = true;
 	openFirewall = true;
